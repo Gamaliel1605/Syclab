902fdd0266900585e81518ef5fd6a6f432af9699
Kane <jehnsen.hirena@sci.ui.ac.id>
2021-10-26 22:59:43 +0700

    finish ui without coloring

---
+++ Syclab/Features/Virtual Lab/GravityPopoverViewController.swift
@@ -0,0 +1,69 @@
+//
+//  GravityPopoverViewController.swift
+//  syclab
+//
+//  inspired from Nicholas and adapted by Kane on 26/10/21.
+//
+
+import UIKit
+
+enum Planet: String {
+    case earth = "Bumi"
+    case moon = "Bulan"
+    case mars = "Mars"
+    case jupiter = "Jupiter"
+    
+    func getGravityValue() -> Double {
+        switch self {
+        case .earth:
+            return 10
+        case .moon:
+            return 2
+        case .mars:
+            return 4
+        case .jupiter:
+            return 25
+        }
+    }
+}
+
+class GravityPopoverViewController: UITableViewController {
+    let chosenPlanet: Planet? = nil
+    var parentTable: UITableView?
+    var parentIndexPath: IndexPath?
+    let planets: [Planet] = [.earth, .moon, .mars, .jupiter]
+    var delegate: GravityPopoverDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return planets.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let planet = planets[indexPath.row]
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: nil)
+        cell.textLabel?.text = planet.rawValue
+        cell.detailTextLabel?.text = "\(planet.getGravityValue()) m/s²"
+        cell.accessoryType = .none
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Chosen planet: \(planets[indexPath.row])")
+        // Should dismiss
+//        parentTable?.deselectRow(at: parentIndexPath!, animated: true)
+        self.delegate?.chooseGravity(chosenValue: self.planets[indexPath.row])
+        print(self.planets[indexPath.row])
+        self.dismiss(animated: true)
+    }
+}
+
+protocol GravityPopoverDelegate: AnyObject {
+    func chooseGravity(chosenValue planet: Planet)
+}
+
--- Syclab/Features/Virtual Lab/VirtualLabController.swift
+++ Syclab/Features/Virtual Lab/VirtualLabController.swift
@@ -6,24 +6,101 @@
 //
 
 import UIKit
-
-class VirtualLabController: UIViewController {
-
+import SpriteKit
+class VirtualLabController: UIViewController, GravityPopoverDelegate {
+   
+    @IBOutlet weak var instructionButton: UIButton!
+    @IBOutlet weak var infoButton: UIButton!
+    @IBOutlet weak var jalankanButton: UIButton!
+    @IBOutlet weak var gravitationButton: UIButton!
+    @IBOutlet weak var varLbl1: UILabel!
+    @IBOutlet weak var varSlider1: UISlider!
+    @IBOutlet weak var varLbl2: UILabel!
+    @IBOutlet weak var varSlider2: UISlider!
+    @IBOutlet weak var varTextField2: UITextField!
+    @IBOutlet weak var varTextField1: UITextField!
+    @IBOutlet weak var unitLbl1: UILabel!
+    @IBOutlet weak var unitLbl2: UILabel!
+    @IBOutlet weak var spriteView: SKView!
+    let button:UIButton = UIButton(type: UIButton.ButtonType.custom)
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.topItem?.title = "Eksperimen Gerak Parabola"
+        setupControlPanel()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Pressed Button Function
+    
+    @objc func buttonAction(sender: UIButton!) {
+            print("oi asw")
     }
-    */
 
+    @IBAction func gravitationBtnPressed(_ sender: Any) {
+        let gravityChoice = GravityPopoverViewController()
+        gravityChoice.delegate = self
+        gravityChoice.modalPresentationStyle = .popover
+        gravityChoice.preferredContentSize = CGSize(width: 200, height: 200)
+        let popover: UIPopoverPresentationController = gravityChoice.popoverPresentationController!
+        popover.sourceView = gravitationButton
+        self.present(gravityChoice, animated: true, completion: nil)
+        print("anjay")
+    }
+    @IBAction func jalankanBtnPressed(_ sender: Any) {
+    }
+    
+    // MARK: - SETUP UI
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    func setupGravitationButton() {
+        
+    }
+    
+    func setupTheoryButton() {
+        let imageSize:CGSize = CGSize(width: 50, height: 50)
+        button.frame = CGRect(x: (jalankanButton.layer.position.x) - 35, y: (jalankanButton.layer.position.y + (1-0.284173) * view.bounds.height) + 35 + 15, width: 70, height: 70)
+        button.setImage(UIImage(systemName: "book.closed.fill"), for: UIControl.State.normal)
+        button.contentVerticalAlignment = .fill
+        button.contentHorizontalAlignment = .fill
+        button.imageEdgeInsets = UIEdgeInsets(
+            top: (button.frame.size.height - imageSize.height) / 4,
+            left: (button.frame.size.width - imageSize.width) / 4,
+            bottom: (button.frame.size.height - imageSize.height) / 4,
+            right: (button.frame.size.width - imageSize.width) / 4)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    func setupControlPanel() {
+        setupGravitationButton()
+        setupTheoryButton()
+        setupIntrucButton()
+    }
+    
+    func setupIntrucButton() {
+        instructionButton.setTitle("", for: .normal)
+    }
+    
+    func setupInfoButon() {
+//        let imageSize:CGSize = CGSize(width: 100, height: 100)
+//        infoButton.setImage(UIImage(systemName: "info.circle"), for: .normal)
+//        infoButton.contentVerticalAlignment = .fill
+//        infoButton.contentHorizontalAlignment = .fill
+//
+//        infoButton.imageEdgeInsets = UIEdgeInsets(
+//            top: (infoButton.frame.size.height - imageSize.height) / 4,
+//            left: (infoButton.frame.size.width - imageSize.width) / 4,
+//            bottom: (infoButton.frame.size.height - imageSize.height) / 4,
+//            right: (infoButton.frame.size.width - imageSize.width) / 4)
+//        infoButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+    }
+
+    
+    // MARK: - PROTOCOL
+    func chooseGravity(chosenValue planet: Planet) {
+        gravitationButton.setTitle(planet.rawValue + " \(planet.getGravityValue()) m/s²", for: .normal)
+        print("juancog")
+    }
 }
