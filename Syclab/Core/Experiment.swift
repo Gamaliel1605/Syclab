eca1077290ee5ba66cdd85380f1e8557c5a18175
Carlos Gamaliel Manurung <carlosgamaliel>
2021-10-29 23:30:22 +0700

    Create class data

---
+++ Syclab/Core/Experiment.swift
@@ -0,0 +1,20 @@
+//
+//  Experiment.swift
+//  Syclab
+//
+//  Created by Carlos Gamaliel Manurung on 29/10/21.
+//
+
+import Foundation
+import UIKit
+
+class Experiment {
+    
+    var expTitle: String
+    var expImage: String
+    
+    init(expTitle: String, expImage: String) {
+        self.expTitle = expTitle
+        self.expImage = expImage
+    }
+}
