73fdaeaf0b78794c980df3fec9c2f4f936c7d9af
oddesa <jehnsen.hirena@sci.ui.ac.id>
2021-11-06 20:04:13 +0700

    update instruction and theory functionality

--- Syclab/Features/Virtual Lab/VirtualLabController.swift
+++ Syclab/Features/Virtual Lab/VirtualLabController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 import SpriteKit
+
+
+
+
 class VirtualLabController: UIViewController, GravityPopoverDelegate {
     
     @IBOutlet weak var infoButton: UIButton!
@@ -36,13 +40,63 @@ class VirtualLabController: UIViewController, GravityPopoverDelegate {
         setupSpriteView()
         setupScene()
         setupControlPanel()
+        
+        self.navigationItem.hidesBackButton = true 
+        let newBackButton = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.back(sender:)))
+        self.navigationItem.leftBarButtonItem = newBackButton
     }
     // MARK: - Pressed Button Function
     
-    //    @objc func playButtonPressed(sender: UIButton!) {
-    //        guard let gameScene = spriteView.scene as? GerakParabolaScene else {return}
-    //        gameScene.shootStraight()
-    //    }
+    @objc func back(sender: UIBarButtonItem) {
+        let exitAlert = Exit()
+        exitAlert.delegate = self
+        self.present(exitAlert, animated: true, completion: nil)
+
+        }
+    
+    @IBAction func instructionButtonPressed(_ sender: Any) {
+        let labInstructionsView = LabInstructions(frame: CGRect(x: self.view.bounds.width - (367+20),
+                                                                y: 40,
+                                                                width: 367,
+                                                                height: 527)
+        )
+        labInstructionsView.delegate = self
+        labInstructionsView.tag = 100
+        self.view.addSubview(labInstructionsView)
+        
+        let labInstructionsContentView = UIView()
+        labInstructionsView.scrollView.addSubview(labInstructionsContentView)
+        labInstructionsContentView.translatesAutoresizingMaskIntoConstraints = false
+        labInstructionsContentView.topAnchor.constraint(equalTo: labInstructionsView.scrollView.topAnchor).isActive = true
+        labInstructionsContentView.leadingAnchor.constraint(equalTo: labInstructionsView.scrollView.leadingAnchor).isActive = true
+        labInstructionsContentView.trailingAnchor.constraint(equalTo: labInstructionsView.scrollView.trailingAnchor).isActive = true
+        labInstructionsContentView.bottomAnchor.constraint(equalTo: labInstructionsView.scrollView.bottomAnchor).isActive = true
+        labInstructionsContentView.widthAnchor.constraint(equalTo: labInstructionsView.scrollView.widthAnchor).isActive = true
+        
+        var elements = [UIView]()
+        
+        guard let experiment = virtualLabVM?.experiment else {return}
+        let labInstructionsContents = experiment.getLabInstructions().labInstructions
+        for (index, labInstructionsContent) in labInstructionsContents.enumerated() {
+            if(labInstructionsContent is ContentImage) {
+                let imageView = (labInstructionsContent as! ContentImage).create(
+                    elementsContainer: labInstructionsContentView,
+                    elementIndex: index,
+                    elements: elements,
+                    lastElementIndex: labInstructionsContents.endIndex - 1
+                )
+                elements.append(imageView)
+            } else if (labInstructionsContent is ContentLabel) {
+                let label = (labInstructionsContent as! ContentLabel).create(
+                    elementsContainer: labInstructionsContentView,
+                    elementIndex: index,
+                    elements: elements,
+                    lastElementIndex: labInstructionsContents.endIndex - 1
+                )
+                elements.append(label)
+            }
+        }
+    }
     
     @IBAction func resetButtonPressed(_ sender: Any) {
         guard let scene = spriteScene as? GerakParabolaScene else {return}
@@ -80,6 +134,53 @@ class VirtualLabController: UIViewController, GravityPopoverDelegate {
         gameScene.shootStraight()
     }
     @IBAction func theoryButtonPressed(_ sender: Any) {
+        let backgroundView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
+        backgroundView.backgroundColor = .black.withAlphaComponent(0.3)
+        backgroundView.tag = 100
+        self.view.addSubview(backgroundView)
+        
+        let conceptTheoryView = ConceptTheory(frame: CGRect(x: self.view.center.x - (843/2),
+                                                            y: self.view.center.y - (620/2),
+                                                            width: 843,
+                                                            height: 620)
+        )
+        conceptTheoryView.delegate = self
+        
+        backgroundView.addSubview(conceptTheoryView)
+        
+        let conceptTheoryContentView = UIView()
+        conceptTheoryView.scrollView.addSubview(conceptTheoryContentView)
+        conceptTheoryContentView.translatesAutoresizingMaskIntoConstraints = false
+        conceptTheoryContentView.topAnchor.constraint(equalTo: conceptTheoryView.scrollView.topAnchor).isActive = true
+        conceptTheoryContentView.leadingAnchor.constraint(equalTo: conceptTheoryView.scrollView.leadingAnchor).isActive = true
+        conceptTheoryContentView.trailingAnchor.constraint(equalTo: conceptTheoryView.scrollView.trailingAnchor).isActive = true
+        conceptTheoryContentView.bottomAnchor.constraint(equalTo: conceptTheoryView.scrollView.bottomAnchor).isActive = true
+        conceptTheoryContentView.widthAnchor.constraint(equalTo: conceptTheoryView.scrollView.widthAnchor).isActive = true
+        
+        var elements = [UIView]()
+        
+        guard let experiment = virtualLabVM?.experiment else {return}
+        let conceptTheoryContents = experiment.getConceptTheory().conceptTheory
+        for (index, conceptTheoryContent) in conceptTheoryContents.enumerated() {
+            if (conceptTheoryContent is ContentImage) {
+                let imageView = (conceptTheoryContent as! ContentImage).create(
+                    elementsContainer: conceptTheoryContentView,
+                    elementIndex: index,
+                    elements: elements,
+                    lastElementIndex: conceptTheoryContents.endIndex - 1
+                )
+                elements.append(imageView)
+            } else if (conceptTheoryContent is ContentLabel) {
+                let label = (conceptTheoryContent as! ContentLabel).create(
+                    elementsContainer: conceptTheoryContentView,
+                    elementIndex: index,
+                    elements: elements,
+                    lastElementIndex: conceptTheoryContents.endIndex - 1
+                )
+                elements.append(label)
+            }
+        }
+
         
     }
     
@@ -328,6 +429,18 @@ extension VirtualLabController: FinishAlertProtocol {
     func onTapToQuiz() {
         print("kuis belom jadi")
     }
-    
-    
+}
+
+extension VirtualLabController: ExitAlertProtocol {
+    func onTapKeluar() {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension VirtualLabController: DismissProtocol {
+    func dismissView() {
+        if let viewWithTag = self.view.viewWithTag(100) {
+            viewWithTag.removeFromSuperview()
+        }
+    }
 }
