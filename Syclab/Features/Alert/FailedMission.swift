3f3078914fd4c3c1fc5171aeb7ee48d1e61f4e5d
Ivan Kelvin <ivankelvin29@gmail.com>
2021-12-09 20:34:05 +0700

    Update FailedMission.swift Fix backButton bug for HukumNewtonVLab

--- Syclab/Features/Alert/FailedMission.swift
+++ Syclab/Features/Alert/FailedMission.swift
@@ -13,10 +13,11 @@ class FailedMission: UIViewController {
     @IBOutlet weak var failedView: UIView!
     @IBOutlet weak var imageFailed: UIImageView!
     @IBOutlet weak var labelFailed: UILabel!
+    var text: String!
     
     override func viewDidLoad() {
-        
-        failedView.layer.cornerRadius = 17
         super.viewDidLoad()
+        failedView.layer.cornerRadius = 17
+        labelFailed.text = text
     }
 }
